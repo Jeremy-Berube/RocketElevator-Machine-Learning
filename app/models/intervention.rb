28a64eeef8050aa2ee3d91e256d39cf09541e6c2
DaveVaval <grimmjowx9@hotmail.com>
2021-03-25 19:37:38 -0400

    Tweaks to the hide show logic

--- app/models/intervention.rb
+++ app/models/intervention.rb
@@ -1,6 +1,6 @@
 class Intervention < ApplicationRecord
     belongs_to :building 
-    belongs_to :battery
+    belongs_to :battery, optional: true
     belongs_to :column, optional: true
     belongs_to :elevator, optional: true
     belongs_to :employee, optional: true
