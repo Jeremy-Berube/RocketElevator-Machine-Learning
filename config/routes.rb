5f1433a6716c49310d35f8cde45ae3984a2a5074
danigrum <danigrum@gmail.com>
2021-04-21 11:22:24 -0400

    Speech: start working on pages and view

--- config/routes.rb
+++ config/routes.rb
@@ -10,12 +10,13 @@ Rails.application.routes.draw do
   get 'pages/residential'
   get 'pages/quote'
   get 'pages/intervention'
+  get 'pages/speech'
   get 'update_buildings', to: 'interventions#update_buildings'
   get 'update_battery', to: 'interventions#update_battery'
   get 'update_column', to: 'interventions#update_column'
   get 'update_elevator', to: 'interventions#update_elevator'
   get 'dropbox/auth_callback' => 'dropbox#auth_callback'
-  
+
 
   resources :quotes
   resources :leads
