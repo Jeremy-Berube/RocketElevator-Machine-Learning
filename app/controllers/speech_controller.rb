25f74e2b219591c2fa1a96a513d4845e6f826f49
Jeremy-Berube <sylfe94@hotmail.com>
2021-04-21 20:10:03 -0400

    First Commit

--- app/controllers/speech_controller.rb
+++ app/controllers/speech_controller.rb
@@ -1,2 +1,44 @@
 class SpeechController < ApplicationController
+    
+    # GET Get Transcription
+    require 'net/http'
+
+    uri = URI('https://westus.api.cognitive.microsoft.com/speechtotext/v3.0/transcriptions/{id}')
+    uri.query = URI.encode_www_form({
+    })
+
+    request = Net::HTTP::Get.new(uri.request_uri)
+    # Request headers
+    request['Ocp-Apim-Subscription-Key'] = '{subscription key}'
+    # Request body
+    request.body = "{body}"
+
+    response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+        http.request(request)
+    end
+
+    puts response.body
+
+
+
+    # POST Create Transcription
+    require 'net/http'
+
+    uri = URI('https://westus.api.cognitive.microsoft.com/speechtotext/v3.0/transcriptions')
+    uri.query = URI.encode_www_form({
+    })
+    
+    request = Net::HTTP::Post.new(uri.request_uri)
+    # Request headers
+    request['Content-Type'] = 'application/json'
+    # Request headers
+    request['Ocp-Apim-Subscription-Key'] = '{subscription key}'
+    # Request body
+    request.body = "{body}"
+    
+    response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+        http.request(request)
+    end
+    
+    puts response.body
 end
